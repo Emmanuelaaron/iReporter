6bb5867747b2741d13c0bc2fa5e59c2423f5b6f4
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2018-11-27 09:46:13 +0300

    User signup endpoint This tests the first name and last name fields are empty [delivers #162193111]

---
+++ tests.py
@@ -0,0 +1,74 @@
+import unittest
+from api import*
+from api.views import app
+from flask import json
+# from api.models import Users
+
+class TestUser(unittest.TestCase):
+
+    def Setup(self):
+        self.tester = app.test_client(self)
+
+    def test_signupuser(self):
+
+        user = dict(
+            firstname="Donald",
+            lastname="ngiya",
+            othernames="Danny",
+            email="ngiya@gams.com",
+            password="ghgdhgd",
+            username="dojo"
+        )
+        resp = app.test_client(self).post(
+            "api/v1/signup", 
+            content_type="application/json",
+            data=json.dumps(user)
+        )
+
+        reply = json.loads(resp.data.decode())
+
+        self.assertIn("You've, signed up sucessfully", reply)
+        self.assertEqual(resp.status_code, 201)
+
+    def test_signupuser_without_firstname(self):
+        user = dict(
+            firstname="",
+            lastname="ngiya",
+            othernames="Danny",
+            email="ngiya@gams.com",
+            password="ghgdhgd",
+            username="dojo"
+        )
+        resp = app.test_client(self).post(
+            "api/v1/signup",
+            content_type="application/json",
+            data = json.dumps(user)
+        )
+
+        reply = json.loads(resp.data.decode())
+
+        self.assertEqual(reply["message"], "firstname is required!")
+        self.assertEqual(resp.status_code, 400)
+    
+    def test_signupuser_without_lastname(self):
+        user = dict(
+            firstname="Donald",
+            lastname=" ",
+            othernames="Danny",
+            email="ngiya@gams.com",
+            password="ghgdhgd",
+            username="dojo"
+        )
+
+        resp = app.test_client(self).post(
+            "api/v1/signup",
+            content_type="application/json",
+            data = json.dumps(user)
+        )
+
+        reply = json.loads(resp.data.decode())
+
+        self.assertEqual(reply["message"], "lastname is required!")
+        self.assertEqual(resp.status_code, 400)
+
+
